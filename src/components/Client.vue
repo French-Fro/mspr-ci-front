641d49219285cad2c8d27109eeeb13950d9dfde2
Unknown <julien.lenoir97@orange.fr>
2020-01-22 11:21:56 +0100

    Update client list UI

--- src/components/Client.vue
+++ src/components/Client.vue
@@ -1,9 +1,12 @@
 <template>
 <div class="container">
     <div>
-        <div class="col-lg-4">
-            <div>
-                {{ firstname }} {{ lastname }}
+        <div class="col-lg-3">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <p class="card-text">{{ firstname }}</p>
+                    <p class="card-text">{{ lastname }}</p>
+                </div>
             </div>
         </div>
     </div>
